5ca3b6cb0b36e6c2e6667618ec39853493f276fc
kawax <kawaxbiz@gmail.com>
2023-02-15 18:56:13 +0900

    add event/list

---
+++ api/event/list.js
@@ -0,0 +1,36 @@
+import {
+    relayInit,
+} from 'nostr-tools'
+
+import 'websocket-polyfill'
+
+export default async function handler(request, response) {
+    const { ids = [], kinds = [1], authors = [], since, until, limit, relay } = request.body
+
+    const filter = {
+        ids: ids,
+        kinds: kinds,
+        authors: authors,
+        since: since,
+        until: until,
+        limit: limit,
+    }
+
+    const relay_server = relayInit(relay)
+    await relay_server.connect()
+
+    relay_server.on('connect', () => {
+        console.log(`connected to ${relay_server.url}`)
+    })
+    relay_server.on('error', () => {
+        console.log(`failed to connect to ${relay_server.url}`)
+    })
+
+    let events = await relay_server.list([filter])
+
+    await relay_server.close()
+
+    return response.status(200).json({
+        events: events,
+    });
+}
