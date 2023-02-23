51eec55f32860397499dafcbae8b6eac476ece7e
kawax <kawaxbiz@gmail.com>
2023-02-23 14:18:48 +0900

    wip

--- api/event/get.ts
+++ api/event/get.ts
@@ -24,7 +24,7 @@ export default async function handler(request: VercelRequest, response: VercelRe
         console.log(`failed to connect to ${relay_server.url}`)
     })
 
-    const event: Event = await relay_server.get(filter, opts)
+    const event = await relay_server.get(filter)
 
     return response.status(200).json({
         event: event,
