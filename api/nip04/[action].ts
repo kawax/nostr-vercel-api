ce31e9d5341df72cdcc2499046ace1464881e28c
kawax <kawaxbiz@gmail.com>
2023-02-25 20:25:50 +0900

    add nip04

---
+++ api/nip04/[action].ts
@@ -0,0 +1,36 @@
+import {nip04} from 'nostr-tools'
+
+import type {VercelRequest, VercelResponse} from '@vercel/node'
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+    const {action}: { action?: string } = request.query
+
+    switch (action) {
+        case 'encrypt':
+            return encrypt(request, response)
+        case 'decrypt':
+            return decrypt(request, response)
+        default:
+            return response.status(404).json({error: 'Not Found'})
+    }
+}
+
+async function encrypt(request: VercelRequest, response: VercelResponse): Promise<VercelResponse> {
+    const {pk, sk, content}: { pk: string, sk: string, content: string } = request.body
+
+    const encrypt: string = await nip04.encrypt(sk, pk, content);
+
+    return response.status(200).json({
+        encrypt: encrypt,
+    })
+}
+
+async function decrypt(request: VercelRequest, response: VercelResponse): Promise<VercelResponse> {
+    const {pk, sk, content}: { pk: string, sk: string, content: string } = request.body
+
+    const decrypt: string = await nip04.decrypt(sk, pk, content);
+
+    return response.status(200).json({
+        decrypt: decrypt,
+    })
+}
