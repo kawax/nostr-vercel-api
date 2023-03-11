4445ee1d2cdde9628a61612852be209d6e234b96
kawax <kawaxbiz@gmail.com>
2023-03-11 20:15:52 +0900

    Create sign.test.ts

---
+++ tests/event/sign.test.ts
@@ -0,0 +1,23 @@
+import {expect, test} from 'vitest'
+
+import sign from '../../api/event/sign'
+
+vi.mock('nostr-tools', () => ({
+    signEvent: (event, key) => 'sign'
+}));
+
+test('event/sign', () => {
+    const req = {
+        body: {
+            event: {},
+            sk: '',
+        }
+    }
+
+    const res = {
+        status: (statusCode) => res,
+        json: (jsonBody) => res,
+    }
+
+    expect(sign(req, res)).toBe(res)
+});
