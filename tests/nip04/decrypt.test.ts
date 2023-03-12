3ec225b7b0a2237878691f12d6f228cff4b4e18d
kawax <kawaxbiz@gmail.com>
2023-03-12 16:11:01 +0900

    Create decrypt.test.ts

---
+++ tests/nip04/decrypt.test.ts
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest'
+
+import handler from '../../api/nip04/[action]'
+import {VercelRequest, VercelResponse} from "@vercel/node";
+
+vi.mock('nostr-tools', () => ({
+    nip04: {
+        decrypt: () => 'test',
+    },
+}))
+
+test('nip04/decrypt', () => {
+    const req = {
+        query: {
+            action: 'decrypt',
+        },
+        body: {
+            pk: 'pk',
+            sk: 'sk',
+            content: 'content',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
+
