050bfc080caf2e30294fcd21d57b7e1a20168670
kawax <kawaxbiz@gmail.com>
2023-03-12 14:46:22 +0900

    Create decode.test.ts

---
+++ tests/nip19/decode.test.ts
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest'
+
+import handler from '../../api/nip19/[action]'
+import {VercelRequest, VercelResponse} from "@vercel/node";
+
+vi.mock('nostr-tools', () => ({
+    nip19: {
+        decode: () => ({
+            type: 'nsec',
+            data: 'test',
+        }),
+    },
+}))
+
+test('nip19/decode', () => {
+    const req = {
+        query: {
+            action: 'decode',
+        },
+        body: {
+            n: 'n',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
