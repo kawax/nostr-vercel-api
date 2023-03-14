afe2ab2b4b4d62b9b91c5c4cdb72953a1fc12031
kawax <kawaxbiz@gmail.com>
2023-03-14 16:32:29 +0900

    Create encode.test.ts

---
+++ tests/nip19/encode.test.ts
@@ -0,0 +1,123 @@
+import { expect, test } from 'vitest'
+
+import handler from '../../api/nip19/[action]'
+import {VercelRequest, VercelResponse} from "@vercel/node";
+
+vi.mock('nostr-tools', () => ({
+    nip19: {
+        nsecEncode: () => 'test',
+        npubEncode: () => 'test',
+        noteEncode: () => 'test',
+        nprofileEncode: () => 'test',
+        neventEncode: () => 'test',
+        naddrEncode: () => 'test',
+    },
+}))
+
+test('nip19/nsec', () => {
+    const req = {
+        query: {
+            action: 'nsec',
+        },
+        body: {
+            sk: 'sk',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
+
+test('nip19/npub', () => {
+    const req = {
+        query: {
+            action: 'npub',
+        },
+        body: {
+            pk: 'pk',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
+
+test('nip19/note', () => {
+    const req = {
+        query: {
+            action: 'note',
+        },
+        body: {
+            note: 'note',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
+
+test('nip19/nprofile', () => {
+    const req = {
+        query: {
+            action: 'nprofile',
+        },
+        body: {
+            profile: 'profile',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
+
+test('nip19/nevent', () => {
+    const req = {
+        query: {
+            action: 'nevent',
+        },
+        body: {
+            event: 'event',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
+
+test('nip19/naddr', () => {
+    const req = {
+        query: {
+            action: 'naddr',
+        },
+        body: {
+            addr: 'addr',
+        }
+    }
+
+    const res = {
+        status: () => res,
+        json: () => res,
+    }
+
+    expect(handler(<VercelRequest><unknown>req, <VercelResponse><unknown>res)).toBeTypeOf('object')
+})
