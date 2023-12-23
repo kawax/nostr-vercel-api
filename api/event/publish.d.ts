89c7ce519c7fb983f33c536330bcb2acaa85b69a
kawax <kawaxbiz@gmail.com>
2023-12-23 21:51:09 +0900

    ts

--- api/event/publish.d.ts
+++
@@ -1,3 +0,0 @@
-import 'websocket-polyfill';
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(request: VercelRequest, response: VercelResponse): Promise<VercelResponse>;
--- api/nip04/[action].d.ts
+++
@@ -1,2 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(request: VercelRequest, response: VercelResponse): Promise<VercelResponse>;
