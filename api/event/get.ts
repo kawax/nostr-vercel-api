0a788f5e09137fe12370f08f8a3018faab4bdf35
kawax <kawaxbiz@gmail.com>
2024-03-20 14:55:14 +0900

    Update get.ts

--- api/event/get.ts
+++ api/event/get.ts
@@ -5,6 +5,8 @@ import type {VercelRequest, VercelResponse} from '@vercel/node';
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     const {filter, relay}: { filter: NDKFilter, relay: string } = request.body
 
+    console.log(filter, relay)
+
     const ndk : NDK = new NDK({
         explicitRelayUrls: [relay],
     });
@@ -12,6 +14,8 @@ export default async function handler(request: VercelRequest, response: VercelRe
     await ndk.connect();
 
     const event : NDKEvent | null = await ndk.fetchEvent(filter);
+
+    console.log(event)
 
     return response.status(200).json({
         event: event,
