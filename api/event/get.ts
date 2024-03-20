db2c7ce277ce2fb71ce928b9227f8bcaa51df358
kawax <kawaxbiz@gmail.com>
2024-03-20 20:19:28 +0900

    Update get.ts

--- api/event/get.ts
+++ api/event/get.ts
@@ -4,11 +4,11 @@ import type {VercelRequest, VercelResponse} from '@vercel/node';
 import type {Event, Filter} from 'nostr-tools'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
-    const {filter, id, relay}: { filter: Filter, id: string, relay: string } = request.body
+    const {filter, relay}: { filter: Filter, relay: string } = request.body
 
     const pool : SimplePool = new SimplePool()
 
-    const event: Event|null = await pool.get([relay], filter, {id: id})
+    const event: Event|null = await pool.get([relay], filter)
 
     return response.status(200).json({
         event: event,
