902ce4a6afe01c6cd2912f49fde367e723b6fca3
kawax <kawaxbiz@gmail.com>
2024-03-20 19:03:48 +0900

    Update publish.ts

--- api/event/publish.ts
+++ api/event/publish.ts
@@ -1,4 +1,4 @@
-import {NostrSystem, EventBuilder, NotSignedNostrEvent, NostrEvent, OkResponse} from "@snort/system";
+import {NostrSystem, EventBuilder, NotSignedNostrEvent, NostrEvent} from "@snort/system";
 
 import type {VercelRequest, VercelResponse} from '@vercel/node';
 
