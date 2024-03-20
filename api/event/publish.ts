9f69c2822a07d65fcb026bce5e0cd279e010bd43
kawax <kawaxbiz@gmail.com>
2024-03-20 12:40:52 +0900

    ws

--- api/event/publish.ts
+++ api/event/publish.ts
@@ -7,13 +7,15 @@ import {
 
 import { hexToBytes } from '@noble/hashes/utils'
 
-useWebSocketImplementation(require('ws'))
+import { WebSocket } from 'ws'
 
 import type {VercelRequest, VercelResponse} from '@vercel/node';
 import type {Event, VerifiedEvent} from 'nostr-tools'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     const {event, sk, relay}: { event: Event, sk: string, relay: string } = request.body
+
+    useWebSocketImplementation(WebSocket)
 
     event.created_at = event.created_at ?? Math.floor(Date.now() / 1000)
 
