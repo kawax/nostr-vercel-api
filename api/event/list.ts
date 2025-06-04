094f94622c4b79e19fd7649b17959e01d02b0493
kawax <kawaxbiz@gmail.com>
2025-06-04 17:19:06 +0900

    Update list.ts

--- api/event/list.ts
+++ api/event/list.ts
@@ -9,13 +9,9 @@ import type {Event, Filter} from 'nostr-tools'
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     const {filter, relay}: { filter: Filter, relay: string } = request.body
 
-    //console.log(filter, relay)
-
     const pool: SimplePool = new SimplePool()
 
     const events: Event[] = await pool.querySync([relay], filter)
-
-    //console.log(events)
 
     return response.status(200).json({
         events: events,
